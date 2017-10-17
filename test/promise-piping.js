1feebda49cd4d0efefb138d8b5a34f52a5a633b3
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2017-10-17 16:22:49 +0200

    Very basic test

--- test/promise-piping.js
+++ test/promise-piping.js
@@ -1,1 +1,23 @@
-console.error('Sorry, no tests yet')
+'use strict'
+
+const t = require('tap')
+require('tap-given')(t)
+
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+chai.should()
+
+Feature('Test promise-piping module', () => {
+  Scenario('Module compiles', () => {
+    let PromisePiping
+
+    When('PromisePiping class is required', () => {
+      PromisePiping = require('../lib/promise-piping').PromisePiping
+    })
+
+    Then('PromisePiping class is ok', () => {
+      return PromisePiping.should.be.ok
+    })
+  })
+})
